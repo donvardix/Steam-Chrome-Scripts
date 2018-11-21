baa3cf6d51dba89e4108fca04b0f3e006e492f47
donvardix <donvardix@gmail.com>
2018-11-21 22:34:37 +0200

    Create steam_awards_nomination.js

---
+++ steam_scrips/steam_awards_nomination.js
@@ -0,0 +1,26 @@
+ShowBlockingWaitDialog("Nominating...", "");
+var Nominations = [
+	578080,
+	342180,
+	505170,
+	21416,
+	268910,
+	218620,
+	612880,
+	244850
+];
+Nominations.forEach(function(Element, Index) {
+	setTimeout(NominationPost, Index * 500, Element, Index + 1);
+});
+setTimeout(function() {
+	document.location.href = "//store.steampowered.com/SteamAwardNominations/";
+}, (Nominations.length + 3) * 500);
+
+function NominationPost(AppId, CategoryId) {
+	$J.post('//store.steampowered.com/steamawards/nominategame', {
+		sessionid: g_sessionID,
+		nominatedid: AppId,
+		categoryid: CategoryId,
+		source: 3,
+	});
+};
